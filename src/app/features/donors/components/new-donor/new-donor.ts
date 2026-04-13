58f68f2d44d0c7183a68293fb59fcde51ff56df7
MostafaAbobaker <35193789+MostafaAbobaker@users.noreply.github.com>
2026-04-13 23:24:35 +0200

    add ui new donor

--- src/app/features/donors/components/new-donor/new-donor.ts
+++ src/app/features/donors/components/new-donor/new-donor.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, signal, Signal } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { NewDonorData } from '../../interface/new-donor-data';
+import { FormField } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-new-donor',
-  imports: [],
+  imports: [TranslatePipe,InputTextModule,FormField],
   templateUrl: './new-donor.html',
   styleUrl: './new-donor.css',
 })
-export class NewDonor {}
+export class NewDonor {
+   
+
+  newDonor= signal<NewDonorData>({
+    nameAr: '',
+    nameEn: '',
+    ID: '',
+    DateOfBirth: new Date(),
+    Gender: '',
+    Occupation: '',
+    MobileNumber: '',
+    Phone: '',
+    Email: '',
+    Address: '',
+    BloodType: '',
+    MedicalHistory: '',
+    Notes: '',
+  });
+  submit(event: Event) {
+    event.preventDefault();
+  }
+}
---
+++ src/app/features/donors/interface/new-donor-data.ts
@@ -0,0 +1,15 @@
+export interface NewDonorData {
+    nameAr: string;
+    nameEn: string;
+    ID: string;
+    DateOfBirth: Date;
+    Gender: string;
+    Occupation: string;
+    MobileNumber: string;
+    Phone: string;
+    Email: string;
+    Address: string;
+    BloodType: string;
+    MedicalHistory: string;
+    Notes: string;
+}
